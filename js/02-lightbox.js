4a3fa033e57febfffd86fcf424034e098d432fe0
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-11 01:43:14 +0200

    task 2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const conteinerGallary = document.querySelector(".gallery");
+const markupImg = createMarkupImg(galleryItems);
+conteinerGallary.insertAdjacentHTML("beforeend", markupImg);
+
+function createMarkupImg(item) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}"
+      </a>`,
+    ""
+  );
+}
+
+const lightbox = new SimpleLightbox(".gallery a");
 
 console.log(galleryItems);
