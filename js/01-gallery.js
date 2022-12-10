4c16bf9d34e401efd3a87e30463195c6840d51ba
Natoilad <109770565+Natoilad@users.noreply.github.com>
2022-12-10 18:38:46 +0200

    markup start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const conteinerGallary = document.querySelector(".gallery");
+const markupImg = createMarkupImg(galleryItems);
+
+function createMarkupImg(item) {
+  return galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      acc +
+      `<div class="gallery"></div>
+    <div class="gallery_item">
+      <a class="gallery_link" href="${original}">
+        <img class="gallery_img" src="${preview}" alt="${description}" data-source="${original}">
+      </a>
+    </div>`,
+    ""
+  );
+}
 console.log(galleryItems);
